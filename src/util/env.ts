26a193df2434c028407759530f645e58c148d929
Marian Zburlea <marian@wowdop.com>
2025-03-12 23:50:20 +0400

    fix: semantic release with tags etc
    Co-authored-by: Alexandru Cercel <cercel.alexandru436@gmail.com>

--- src/util/env.ts
+++ src/util/env.ts
@@ -3,4 +3,10 @@ export const getEnv = () => {
   if (typeof process !== 'undefined') {
     return process.env.NODE_ENV
   }
+
+  if (typeof import.meta !== 'undefined') {
+    return import.meta.env.MODE
+  }
+
+  return 'development'
 }

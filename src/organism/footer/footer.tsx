8074ab2bf9b389e22c5594f3670dcaa87edb78a9
Marian Zburlea <m@wowjob.dev>
2025-05-16 01:16:14 +0400

    feat: improve footer with legal links

--- src/organism/footer/footer.tsx
+++ src/organism/footer/footer.tsx
@@ -1,14 +1,57 @@
-import { Flex, Text, TStyle } from '../..'
+import { Flex, NextLink, Text, TStyle } from '../..'
+
+type TLegalItem = {
+  text?: string
+  label?: string
+}
+type TLegal = {
+  cookie?: TLegalItem
+  term?: TLegalItem
+  privacy?: TLegalItem
+  target?: '_blank' & string
+}
 
 export const Footer = async ({
   copyright,
   theme = 'dark',
+  legal,
 }: {
   copyright: string
+  legal?: TLegal
   theme?: TStyle['theme']
 }) => {
+  let legalList: {
+    href: string
+    label?: string
+  }[] = []
+
+  if (legal) {
+    legalList = [
+      { href: 'cookie', label: legal.cookie?.label },
+      { href: 'term', label: legal.term?.label },
+      { href: 'privacy', label: legal.privacy?.label },
+    ].filter(({ label }) => label)
+  }
+
+  const showLegal = legalList.length > 0
+
   return (
     <Flex theme={theme} mobile={{ padding: 32, alignItems: 'center' }}>
+      {showLegal && (
+        <Flex mobile={{ flexDirection: 'row', justifyContent: 'center' }}>
+          {legalList.map(({ label, href }) => (
+            <NextLink
+              href={`/legal/${href}`}
+              key={href}
+              mobile={{ padding: [8, 16] }}
+              target={legal?.target}
+            >
+              {label}
+            </NextLink>
+          ))}
+        </Flex>
+      )}
+
       <Text>{copyright}</Text>
     </Flex>
   )
